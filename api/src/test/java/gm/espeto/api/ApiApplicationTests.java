6545b716eb5b589608e748f1a49fa6212a7a52e5
Matheus Felippe Rodrigues da Silva <102191436+matheusfelippe1@users.noreply.github.com>
2023-04-26 22:23:35 -0300

    Initial commit

---
+++ api/src/test/java/gm/espeto/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package gm.espeto.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
